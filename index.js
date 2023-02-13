29ca3f0ef476487832107296a2ef524c1116b9a8
Mark <mark3fullen@gmail.com>
2023-02-12 19:25:16 -0500

    Cleaning up. This is a microserv

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3000;
-
-var db = new sqlite3.Database('users.db', (err) => {
-    err ? console.log(err.message) : console.log("connected to db")
-});
-
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)");
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send(`
-        <form action='/adduser' method='post'>
-            <input type='text' name='name' placeholder='Enter Username'>
-            <button type='submit'>Login</button>
-        </form>
-
         <form action='/CtoF' method='post'>
             <label>Temperature in C<label>
             <input type='text' name='name'>
@@ -34,24 +20,12 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/adduser', (req, res) => {
-    var name = req.body.name;
-    db.run("INSERT INTO users (name) VALUES (?)", [name], (err) => {
-        err ? console.error(err) && res.status(500).send("Error adding users") : res.send("User added successfully");
-    });
-});
-
-db.all(`SELECT name FROM users`, [], (err, rows) => {
-    err ? console.log(err.message) : rows.forEach((row) => {console.log(row)});
-  });
-  
-
 app.post('/CtoF', (req, res) => {
     const number = req.body.name;
 
     let final = number * 9/5 + 32
 
-    res.send(`${final}`);
+    res.send(`${final} degrees F`);
 })
 
 app.post('/FtoC', (req, res) => {
@@ -59,7 +33,7 @@ app.post('/FtoC', (req, res) => {
 
     let final = (number - 32) * (5/9)
 
-    res.send(`${final}`);
+    res.send(`${final} degrees C`);
 })
 
 app.listen(port, () => {
